1a61abd8e121c8568e3a0af881a19cacb12070f5
Ehsan Morshedzadeh <ehsan.morshedzadeh@gmail.com>
2024-09-19 13:24:30 +0330

    First try to calculate subsets,

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -2,4 +2,27 @@
 
 var sw = Stopwatch.StartNew();
 
+
+
 Console.WriteLine("Found Solution in {0};",sw.ElapsedMilliseconds);
+
+void CalsulateSubsets(List<int> set)
+{
+    var subsets = new List<List<int>>();
+    subsets.Add(new List<int>());
+    
+    foreach (var i in set)
+    {
+        subsets.Add(new List<int> { i });
+    }
+
+    foreach (var i in set)
+    {
+        foreach (var j in set)
+        {
+            
+        }
+    }
+    
+    subsets.Add(set);
+}
